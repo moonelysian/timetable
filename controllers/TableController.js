e87a8f68e493b4418b0a0a068f58e758e349caa2
moonelysian <dandyblack@naver.com>
2019-11-01 13:50:37 +0900

    tablecontroller 이미 등록된 과목 및 시간대 겹치는 과목 확인

--- controllers/TableController.js
+++ controllers/TableController.js
@@ -5,18 +5,18 @@ const Op = sequelize.Op;
 const createTable = function(req, res){
     const body = req.body;
     const course_start = parseInt(body.course_start)
+    const insertData = {
+        course_code: body.code,
+        course_start: course_start,
+        course_day: body.course_day
+    }
     try{
-        if(checkTable(body.code)){
-            models.Timetable.create(
-                { 
-                    course_code: body.code,
-                    course_start: course_start,
-                    course_day: body.course_day
-                }
-            ) 
-            .then(result => res.send({message: '등록되었습니다'}))
+        if(checkTable(insertData)){
+            models.Timetable.create(insertData) 
+            .then(result => res.redirect('/'))
         }
         else{
+            console.log('AAAAAAA')
             res.send({ message: '이미 등록된 과목입니다' });
         }
     }catch(err){
@@ -24,21 +24,29 @@ const createTable = function(req, res){
     }
 }
 
-const checkTable = function(code){
-    try{
-        models.Timetable.findOne({where: {course_code: code}})
-        .then(result => {
-            if(result){
-                console.log(result);
-                return false
-            }
-            else{
-                return true
-            }
-        })
-    }catch(err){
-        console.log(err);
-    }    
+let checkTable = function(insertData){
+    models.Timetable.findOne({where: {course_code: insertData.course_code}})
+    .then(result => {
+        if(result){
+            return false;
+        }
+        else{
+            models.Courses.findOne({where: {
+                [Op.and]:[
+                    {
+                        start_time: insertData.course_start
+                    },
+                    {
+                        dayofweek: {[Op.like]: "%" + insertData.course_day + "%"}
+                    }
+                ]
+            }})
+            .then(result => {
+                if(result.code == insertData.course_code) return false;
+                else return true;
+            })
+        }
+    })
 }
 
 module.exports = {
