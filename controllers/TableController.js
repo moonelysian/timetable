97371c608eaef20b355a049e54674da41a5c7d41
moonelysian <dandyblack@naver.com>
2019-11-01 12:17:36 +0900

    checkTable 제대로 안됨

--- controllers/TableController.js
+++ controllers/TableController.js
@@ -6,40 +6,39 @@ const createTable = function(req, res){
     const body = req.body;
     const course_start = parseInt(body.course_start)
     try{
-        checkTable(body.code)
-        .then(result => {
-            if(result){
-                models.Timetable.create(
-                    { 
-                        course_code: body.code,
-                        course_start: course_start,
-                        course_day: body.course_day
-                    }
-                ) 
-                .then(result => res.send({message: '등록되었습니다'}))
-            }
-            else{
-                res.send({ message: '이미 등록된 과목입니다' });
-            }
-        })
+        if(checkTable(body.code)){
+            models.Timetable.create(
+                { 
+                    course_code: body.code,
+                    course_start: course_start,
+                    course_day: body.course_day
+                }
+            ) 
+            .then(result => res.send({message: '등록되었습니다'}))
+        }
+        else{
+            res.send({ message: '이미 등록된 과목입니다' });
+        }
     }catch(err){
         console.log(err)
     }
 }
 
-let checkTable = function(code){
+const checkTable = function(code){
     try{
         models.Timetable.findOne({where: {course_code: code}})
         .then(result => {
-            if(result!==null){
+            if(result){
                 console.log(result);
                 return false
             }
-            else return true
+            else{
+                return true
+            }
         })
-    } catch(err) {
+    }catch(err){
         console.log(err);
-    }
+    }    
 }
 
 module.exports = {
